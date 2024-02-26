274855d8c632a1158bea6f2f59c2693199dbcac7
Jordan Olguin <jdolguin62@gmail.com>
2024-02-26 12:38:04 -0700

    tic tac toe homepage done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Tic Tac Toe</h1>
+      <LoadingScreen />
     </div>
   );
 }
