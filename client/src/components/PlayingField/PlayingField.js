ab99bef3a2d172191e961a7bc83699846936b08a
Jordan Olguin <jdolguin62@gmail.com>
2024-02-26 17:29:29 -0700

    playing field first draft

---
+++ client/src/components/PlayingField/PlayingField.js
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+import "./PlayingField.css";
+
+const PlayingField = () => {
+  const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i) => {
+      const delay = 1 + i * 0.5;
+      return {
+        pathLength: 1,
+        opacity: 1,
+        transition: {
+          pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+          opacity: { delay, duration: 0.01 },
+        },
+      };
+    },
+  };
+
+  return (
+    <motion.svg
+      width="600"
+      height="600"
+      viewBox="0 0 600 600"
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.line
+        x1="200"
+        y1="100"
+        x2="200"
+        y2="500"
+        stroke="#000"
+        strokeWidth="5"
+        variants={draw}
+        custom={1}
+      />
+      <motion.line
+        x1="400"
+        y1="100"
+        x2="400"
+        y2="500"
+        stroke="#000"
+        strokeWidth="5"
+        variants={draw}
+        custom={1.5}
+      />
+      <motion.line
+        x1="100"
+        y1="200"
+        x2="500"
+        y2="200"
+        stroke="#000"
+        strokeWidth="5"
+        variants={draw}
+        custom={2}
+      />
+      <motion.line
+        x1="100"
+        y1="400"
+        x2="500"
+        y2="400"
+        stroke="#000"
+        strokeWidth="5"
+        variants={draw}
+        custom={2.5}
+      />
+    </motion.svg>
+  );
+};
+
+export default PlayingField;
