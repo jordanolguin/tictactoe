5851c5ccdd34a8b6fc3c4052defa9dd4ba6042e5
Jordan Olguin <jdolguin62@gmail.com>
2024-02-28 20:33:44 -0700

    updated playing field transition

--- client/src/components/PlayingField/PlayingField.js
+++ client/src/components/PlayingField/PlayingField.js
@@ -18,8 +18,26 @@ const PlayingField = () => {
     },
   };
 
+  const pageVariants = {
+    initial: { opacity: 0 },
+    in: { opacity: 1 },
+    out: { opacity: 0 },
+  };
+
+  const pageTransition = {
+    type: "tween",
+    ease: "anticipate",
+    duration: 0.5,
+  };
+
   return (
-    <>
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+    >
       <motion.svg
         width="600"
         height="600"
@@ -69,7 +87,7 @@ const PlayingField = () => {
         />
       </motion.svg>
       <BackButton />
-    </>
+    </motion.div>
   );
 };
 
