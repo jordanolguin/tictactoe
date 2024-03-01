632e97b04d7787b31f9fb829d043da02ec060aff
Jordan Olguin <jdolguin62@gmail.com>
2024-02-29 17:47:26 -0700

    updated back button styles to be inline with play button

--- client/src/components/BackButton/BackButton.js
+++ client/src/components/BackButton/BackButton.js
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
-import "./BackButton.css";
+import { motion, MotionConfig } from "framer-motion";
+import styles from "./BackButton.module.css";
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const [isHover, setIsHover] = useState(false);
+
+  const transition = { type: "spring", stiffness: 400, damping: 17 };
+  const buttonVariants = {
+    rest: { scale: 1, backgroundColor: "#acc7ed" },
+    hover: { scale: 1.5, backgroundColor: "#61dafb" },
+    press: { scale: 1.4, backgroundColor: "#db07d1" },
+  };
 
   return (
-    <motion.button
-      className="box"
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 17 }}
-      onClick={() => navigate("/")}
-    >
-      Back to Home
-    </motion.button>
+    <div className={styles.backButtonContainer}>
+      <MotionConfig transition={transition}>
+        <motion.button
+          className={styles.button}
+          initial="rest"
+          animate={isHover ? "hover" : "rest"}
+          whileHover="hover"
+          whileTap="press"
+          variants={buttonVariants}
+          onHoverStart={() => setIsHover(true)}
+          onHoverEnd={() => setIsHover(false)}
+          onTap={() => navigate("/")}
+        >
+          back to home
+        </motion.button>
+      </MotionConfig>
+    </div>
   );
 };
 
