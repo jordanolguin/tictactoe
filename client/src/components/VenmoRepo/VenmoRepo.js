05f109e5878594140e941c47d4c9d523a666482f
Jordan Olguin <jdolguin62@gmail.com>
2024-03-10 23:29:58 -0600

    updated icons and neumorphism

--- client/src/components/VenmoRepo/VenmoRepo.js
+++ client/src/components/VenmoRepo/VenmoRepo.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faCcApplePay } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 
 const VenmoRepo = () => {
+  const [isPressed, setIsPressed] = useState({
+    github: false,
+    dollar: false,
+  });
+
   const containerStyle = {
     display: "flex",
     justifyContent: "space-around",
@@ -16,22 +23,34 @@ const VenmoRepo = () => {
     cursor: "pointer",
   };
 
-  const buttonStyle = {
+  const getButtonStyle = (pressed) => ({
     display: "inline-flex",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: "10px",
     padding: "20px",
     backgroundColor: "#1b1c1e",
-    boxShadow: `8px 8px 15px #0a0b0c,
-                -8px -8px 15px #2c2d2f`,
+    boxShadow: pressed
+      ? `inset 8px 8px 15px #0a0b0c, inset -8px -8px 15px #2c2d2f`
+      : `8px 8px 15px #0a0b0c, -8px -8px 15px #2c2d2f`,
     cursor: "pointer",
+    transition: "box-shadow 0.2s ease-in-out",
+  });
+
+  const handleButtonClick = (button) => {
+    setIsPressed((prevState) => ({
+      ...prevState,
+      [button]: !prevState[button],
+    }));
   };
 
   return (
     <div style={containerStyle}>
       <div
-        style={buttonStyle}
+        style={getButtonStyle(isPressed.github)}
+        onMouseDown={() => handleButtonClick("github")}
+        onMouseUp={() => handleButtonClick("github")}
+        onMouseLeave={() => isPressed.github && handleButtonClick("github")}
         onClick={() =>
           window.open("https://github.com/jordanolguin/tictactoe", "_blank")
         }
@@ -39,10 +58,13 @@ const VenmoRepo = () => {
         <FontAwesomeIcon icon={faGithub} style={iconStyle} />
       </div>
       <div
-        style={buttonStyle}
+        style={getButtonStyle(isPressed.dollar)}
+        onMouseDown={() => handleButtonClick("dollar")}
+        onMouseUp={() => handleButtonClick("dollar")}
+        onMouseLeave={() => isPressed.dollar && handleButtonClick("dollar")}
         onClick={() => window.open("https://venmo.com/jordanolguin", "_blank")}
       >
-        <FontAwesomeIcon icon={faCcApplePay} style={iconStyle} />
+        <FontAwesomeIcon icon={faMoneyBill} style={iconStyle} />
       </div>
     </div>
   );
