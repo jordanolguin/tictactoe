360848e51bcf6c759d69973cf33a9ff45b38c163
Jordan Olguin <jdolguin62@gmail.com>
2024-03-16 13:49:14 -0600

    updated landing page to accept theme context

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,10 +1,13 @@
 import ThreeJSStar from "../../components/ThreeJSStar/ThreeJSStar";
 import BackButton from "../../components/BackButton/BackButton";
+import { useTheme } from "../../contexts/ThemeContext";
 import "./Landing.css";
 
 const Landing = () => {
+  const { isDark } = useTheme();
+
   return (
-    <div className="landingContainer">
+    <div className={`landingContainer ${isDark ? "dark" : "light"}`}>
       <ThreeJSStar />
       <BackButton />
     </div>
